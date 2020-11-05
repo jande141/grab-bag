837145174e441fe0e60a94dfe65652021ce7286f
Justin Anderson <jande141@calpoly.edu>
2020-11-05 11:16:25 -0800

    formatted code using Prettier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 //import React in our code
-import React, { useState, useEffect } from 'react';
-import { DragImage } from './DragImage';
-import { GrabBag } from './GrabBag';
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
+import React, { useState, useEffect } from "react";
+import { DragImage } from "./DragImage";
+import { GrabBag } from "./GrabBag";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 //import all the components we are going to use
 import {
@@ -15,7 +15,7 @@ import {
   StyleSheet,
   FlatList,
   ActivityIndicator,
-} from 'react-native';
+} from "react-native";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -25,13 +25,12 @@ const App = () => {
   useEffect(() => getData(), []);
 
   const getData = () => {
-
     var limit = 10;
     setLoading(true);
     callApi(limit, offset)
-      .then(response => {
+      .then((response) => {
         setDataSource([...dataSource, ...response]);
-        setOffset(offset + limit);       //Increasing the offset for the next API call
+        setOffset(offset + limit); //Increasing the offset for the next API call
         setLoading(false);
       })
       .catch((error) => {
@@ -40,15 +39,21 @@ const App = () => {
   };
 
   async function callApi(limit, offset) {
-    const categories = await fetch('https://www.ifixit.com/api/2.0/categories/all?limit=' + limit + '&offset=' + offset)
-      .then(response => response.json());
+    const categories = await fetch(
+      "https://www.ifixit.com/api/2.0/categories/all?limit=" +
+        limit +
+        "&offset=" +
+        offset
+    ).then((response) => response.json());
 
     var items = [];
     for (var i = 0; i < categories.length; i++) {
       var cat = categories[i];
 
-      const catInfo = await fetch('https://www.ifixit.com/api/2.0/wikis/CATEGORY/' + encodeURIComponent(cat))
-        .then(response => response.json());
+      const catInfo = await fetch(
+        "https://www.ifixit.com/api/2.0/wikis/CATEGORY/" +
+          encodeURIComponent(cat)
+      ).then((response) => response.json());
 
       try {
         var item = {};
@@ -70,12 +75,11 @@ const App = () => {
           activeOpacity={0.9}
           onPress={getData}
           //On Click of button load more data
-          style={styles.loadMoreBtn}>
+          style={styles.loadMoreBtn}
+        >
           <Text style={styles.btnText}>Load More</Text>
           {loading ? (
-            <ActivityIndicator
-              color="white"
-              style={{ marginLeft: 8 }} />
+            <ActivityIndicator color="white" style={{ marginLeft: 8 }} />
           ) : null}
         </TouchableOpacity>
       </View>
@@ -85,9 +89,7 @@ const App = () => {
   const ItemView = ({ item }) => {
     //console.log('itemview:"' + item.imageUrl + '"');
 
-    return (
-      <DragImage imageUrl={item.imageUrl} />
-    );
+    return <DragImage imageUrl={item.imageUrl} />;
   };
 
   return (
@@ -106,54 +108,54 @@ const App = () => {
         <View style={styles.container}>
           <GrabBag></GrabBag>
         </View>
-        </DndProvider>
+      </DndProvider>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
+    justifyContent: "center",
     flex: 1,
-    borderStyle: 'solid',
-    borderColor: '#000000',
+    borderStyle: "solid",
+    borderColor: "#000000",
     borderWidth: 2,
   },
   footer: {
     padding: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
   },
   loadMoreBtn: {
     padding: 10,
-    backgroundColor: '#800000',
+    backgroundColor: "#800000",
     borderRadius: 4,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
   btnText: {
-    color: 'white',
+    color: "white",
     fontSize: 15,
-    textAlign: 'center',
+    textAlign: "center",
   },
   text: {
-    color: 'red',
+    color: "red",
     fontSize: 15,
-    textAlign: 'center',
-    fontWeight: 'bold'
+    textAlign: "center",
+    fontWeight: "bold",
   },
   itemContainer: {
     height: 110,
     width: 110,
     margin: 10,
-    backgroundColor: '#FFF',
+    backgroundColor: "#FFF",
     padding: 10,
     borderRadius: 6,
   },
   image: {
-    height: '100%',
+    height: "100%",
     borderRadius: 4,
   },
   thumbnail: {
@@ -164,4 +166,3 @@ const styles = StyleSheet.create({
 });
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM, { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM, { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
